878cc13e36ca61717e6f14da0f9eb9810ef25d3b
okb0217 <scandal217.@gmail.com>
2020-03-17 09:03:26 +0000

    [Update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root "products#top"
+
+	resource :end_users, only: [:edit, :update]
+	get "my_page" => "end_users#my_page"
+	get "check" => "end_users#check"
+	post "unsubscribed" => "end_users#unsubscribed"
+
+	resources :products, only: [:index, :show]
+
+	resources :cart_products, only: [:index, :create, :destroy]
+	delete "cart_products" => "cart_products#clear"
+
+	resources :orders, only: [:index, :show, :create]
+	get "input" => "orders#input"
+	get "check" => "orders#check"
+	get "thanks" => "orders#thanks"
+
+	resources :shipping_addresses, only: [:index, :create, :edit, :update, :destroy]
+
+	namespace :admin do
+		get "top" => "orders#top"
+		resources :end_users, only: [:index, :show, :edit, :update]
+		resources :products, only: [:index, :new, :show, :create, :edit, :update]
+		resources :genres, only: [:index, :create, :edit, :update]
+		resources :orders, only: [:index, :show,]
+		post "update" => "orders#update"
+		post "update" => "order_products#update"
+	end
+
 end
