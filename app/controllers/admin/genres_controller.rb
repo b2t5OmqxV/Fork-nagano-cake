42c02f05adbd132b13601104f801896cf9b5747b
DarksideJP <darknebula1419@gmail.com>
2020-03-18 11:05:33 +0000

    [Add] genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,21 @@
 class Admin::GenresController < ApplicationController
+
+	def index
+		@genre = Genre.new
+		@genres = Genre.all
+	end
+
+	def create
+		@genre = Genre.new(genre_params)
+		if @genre.save
+			redirect_to admin_products_path
+		else
+			render :index
+		end
+	end
+
+	private
+	def genre_params
+		params.require(:genre).permit(:name, :is_invalid)
+	end
 end
