0b06f6e9c6d5a270536c012dcb67c121a492d982
DarksideJP <darknebula1419@gmail.com>
2020-03-18 07:38:40 +0000

    doing

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,16 @@
 class Admin::ProductsController < ApplicationController
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		if product.save
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
 end
+
