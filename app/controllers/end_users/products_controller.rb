6707fffbe36fd46e1c4293a2e2a60fed9a65a0d7
okb0217 <scandal217.@gmail.com>
2020-03-19 03:31:05 +0000

    [Update]doing_products_index

--- app/controllers/end_users/products_controller.rb
+++ app/controllers/end_users/products_controller.rb
@@ -1,12 +1,18 @@
-class ProductsController < ApplicationController
+class EndUsers::ProductsController < ApplicationController
 
 	def index
-		@genre = Genre.find(params[:id])
+		@genres = Genre.all
+		if @genre = Genre.find(params[:id])
+		@products = Genre.product.all
+		else
 		@products = Product.all
+		end
 	end
 
 	def show
+		@genres = Genre.all
 		@product = Product.find(params[:id])
 	end
 end
+
 
