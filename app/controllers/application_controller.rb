43250a0aeaf0e37bf7fc3289928823e8d381b270
ryoichi7128 <ryoichi7128@gmail.com>
2020-03-19 07:49:29 +0000

    [Add]end_user 新規登録ページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
 	
+
+
+	# def after_sign_in_path_for(resource)
+ #    new_end_user_session_path
+ #      case resource
+ #      when EndUser
+ #        new_end_user_session_path
+ #      when admin
+ #        admin_root_path
+      # end
+ #  end
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_kana, :first_kana, :zip_code, :address, :phone_number])
+  end
 end
