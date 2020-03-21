6e2ff4bcf24bb1c9fd57ce2be988b91debe6f699
DarksideJP <darknebula1419@gmail.com>
2020-03-21 11:31:26 +0000

    [Add] order-doing

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
 	belongs_to :end_user
 	has_many :order_products
+	enum payment_method: {credit_card: 0, Bank_transfer: 1}
+	enum order_status: {waiting_payment: 0, }
 end
