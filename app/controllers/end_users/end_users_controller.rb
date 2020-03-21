b0171490577b9146103794c33f25b38e58d2816f
b2t5OmqxV <tatuyaizumi7@gmail.com>
2020-03-21 06:45:18 +0000

    [enduser]userthanks-my_pag

--- app/controllers/end_users/end_users_controller.rb
+++ app/controllers/end_users/end_users_controller.rb
@@ -1,6 +1,15 @@
 class EndUsersController < ApplicationControlle
 
-	def my_pages
+	def my_pages 
+		@end_user = EndUser.find(params[:id])
 	end
 
+	def edit 
+		@end_user = EndUser.find(params[:id])
+	end
+
+	private 
+	def end_user_params
+    params.require(:end_user).permit(:family_name,:first_name,:family_name_kana,:first_name_kana,:zip_code,:address,:phone_number,:is_unsubscribed)
+  end
 end
