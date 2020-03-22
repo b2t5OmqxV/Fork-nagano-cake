17da75d7bc0a20d8625756b07e5aa93377f4ef41
okb0217 <scandal217.@gmail.com>
2020-03-22 06:40:26 +0000

    [Add]admin/end_users-index,show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_kana, :first_kana, :zip_code, :address, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :zip_code, :address, :phone_number])
   end
 end
