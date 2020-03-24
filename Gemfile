aafaad30a39d17b4f85074d30f7413bd3d152286
ryoichi7128 <ryoichi7128@gmail.com>
2020-03-24 10:59:27 +0000

    [Add]退会ユーザー弾く機能追加

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ gem 'enum_help'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails'
 gem 'pry-rails'
+
+gem "paranoia"
--- app/controllers/end_users/end_users/sessions_controller.rb
+++ app/controllers/end_users/end_users/sessions_controller.rb
@@ -2,6 +2,10 @@
 
 class EndUsers::EndUsers::SessionsController < Devise::SessionsController
 
+
+
+
+
   def after_sign_in_path_for(resource)
         root_path
   end
@@ -10,6 +14,33 @@ class EndUsers::EndUsers::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     new_end_user_session_path # ログアウト後に遷移するpathを設定
   end
+
+
+
+
+
+
+
+
+  #    def reject_user
+  #   @end_user = EndUser.find_by(email: params[:end_user][:email].downcase)
+
+  #   if @user
+  #     if (@user.valid_password?(params[:end_user][:password]) && (@user.active_for_authentication? == false))
+  #       flash[:error] = "退会済みです。"
+  #       redirect_to new_user_session_path
+  #     end
+  #   else
+  #     flash[:error] = "必須項目を入力してください。"
+  #   end
+  # end
+
+
+
+
+
+
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -19,7 +50,6 @@ class EndUsers::EndUsers::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   # def create
-  #   super
   # end
 
   # DELETE /resource/sign_out
