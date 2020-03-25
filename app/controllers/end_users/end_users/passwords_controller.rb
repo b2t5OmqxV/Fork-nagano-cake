d263882433ec0007b571e47120bb14e6f9bb93f0
b2t5OmqxV <tatuyaizumi7@gmail.com>
2020-03-25 08:24:35 +0000

    [stop]

--- app/controllers/end_users/end_users/passwords_controller.rb
+++ app/controllers/end_users/end_users/passwords_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class EndUsers::PasswordsController < Devise::PasswordsController
+class EndUsers::EndUsers::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
   #   super
---
+++ db/migrate/20200325074754_add_devise_create_to_end_users.rb
@@ -0,0 +1,4 @@
+class AddDeviseCreateToEndUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
